181eb8cc34688e601fd6f9bdb923a69010bfe6f2
danielajanceova <danjan01@easv365.dk>
2024-11-04 15:35:12 +0100

    message

--- src/main.ts
+++ src/main.ts
@@ -170,14 +170,7 @@ const initializeColorPicker = (): void => {
 // Initialize color picker on DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
   initializeColorPicker();
-  document.getElementById("clearCompleted")?.addEventListener("click", clearCompletedTodos);
+  
 });
 
-// Step to clear completed todos
-const clearCompletedTodos = (): void => {
-  todos = todos.filter(todo => !todo.completed);
-  renderTodos();
-
-  
-};
 
